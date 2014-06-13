4e31b8bbb60ce1bbee19e0fb62e4b9ed6cea52c2
c_plus_c <dtm.music.producer@gmail.com>
2014-06-13 23:50:00 +0900

    thrfhfdg

--- create_user_account.php
+++ create_user_account.php
@@ -1,3 +1,33 @@
+<?php
+$dsn = "mysql:dbname=uriage;host=localhost"
+$user = "s1313096"
+$password = "!s1313096!"
+
+try
+{
+	$dbh = new PDO($dsn, $user, $password);
+	
+	$dbh->query('SET NAMES utf8');
+	
+	$sql = "select * from hoge";
+	foreach()
+}
+catch(PDOException $e)
+{
+
+}
+	session_start();
+	
+	$errorMessage = "";
+	
+	$newUserId = htmlspecialchars($_POST["userid"], ENT_QUOTES);
+	
+	if(isset($_POST["login"]))
+	{
+	
+	}
+?>
+<!doctype html>
 <html lang="ja">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
@@ -5,7 +35,7 @@
 </head>
 <body>
 <h1>利用者用アカウント作成画面</h1>
-<form action="#" method="post">
+<form action="<?php print($_SERVER['PHP_SELF']) ?>" method="post">
 ID:<input type="text" name="id" /><br />
 パスワード:<input type="password" name="password" /><br />
 パスワード確認:<input type="password" name="confirm" /><br />
