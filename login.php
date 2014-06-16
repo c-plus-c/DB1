bb7a25e6768d5877f1bc14306a5ce0562f8ed3a3
今西　麻人 <s1313096@template.(none)>
2014-06-16 13:13:55 +0900

    ログイン作成中

--- login.php
+++ login.php
@@ -5,7 +5,77 @@
 </head>
 <body>
 <h1>ログイン画面</h1>
+<?php
+$dsn = "mysql:dbname=DBs1313096;host=koala.cc.tsukuba.ac.jp";
+$user = "s1313096";
+$password = "kousen08";
+
+try
+{
+	$dbh = new PDO($dsn, $user, $password);
+	
+	$dbh->query('SET NAMES utf8');
+
+	if($_POST["account_classification"]=="user")
+	{
+		$sql = "select user_id, password from user";
+		$stmt = $dbh->query($sql);
+
+		$succeed=false;
+		while($result = $stmt->fetch(PDO::FETCH_ASSOC)){
+			if($result['user_id'] == $_POST['id'] && $result['password'] == $_POST['password'])
+			{
+				$succeed = true;
+				break;
+			}
+		}
+
+		if($succeed)
+		{
+
+		}
+		else
+		{
+			print "ログイン失敗<br />";
+		}
+
+	}
+	elseif($_POST["account_classification"]=="exhibitor")
+	{
+		$sql = "select exhibitor_id, password from exhibitor";
+		$stmt = $dbh->query($sql);
+
+		$succeed = false;
+
+		while($result = $stmt->fetch(PDO::FETCH_ASSOC)){
+			if($result['exhibitor_id'] == $_POST['id'] && $result['password'] == $_POST['password'])
+			{
+				$succeed = true;
+				break;
+			}
+		}
+
+		if($succeed)
+		{
+
+		}
+		else
+		{
+			print "ログイン失敗<br />";
+		}
+	}
+}
+catch(PDOException $e)
+{
+
+}
+
+$dbh = null;
+?>
 <form action="#" method="post">
+アカウント種別<br />
+<input type="radio" name="account_classification" value="user" checked>利用者
+<input type="radio" name="account_classification" value="exhibitor">出品者<br />
 ID:<input type="text" name="id" /><br />
 パスワード:<input type="password" name="password" /><br />
 <input type="submit" value="ログイン" />
